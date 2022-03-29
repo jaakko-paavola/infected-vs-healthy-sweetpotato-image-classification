1a28d773e620e3cfb8e8e6d224dca239381f59ec
perander <silva.perander@helsinki.fi>
2022-03-29 14:50:05 +0300

    implement first version of vision transformer

--- utils/image_utils.py
+++ utils/image_utils.py
@@ -138,3 +138,20 @@ def convert_color_to_another(image, color1=[255, 255, 255], color2=[0, 0, 0]):
 
   return data
 # %%
+
+def img_to_patch(x, patch_size, flatten_channels=True):
+    """from https://uvadlc-notebooks.readthedocs.io/en/latest/tutorial_notebooks/tutorial15/Vision_Transformer.html
+    Inputs:
+        x - torch.Tensor representing the image of shape [B, C, H, W]
+        patch_size - Number of pixels per dimension of the patches (integer)
+        flatten_channels - If True, the patches will be returned in a flattened format
+                           as a feature vector instead of a image grid.
+    """
+    B, C, H, W = x.shape
+    x = x.reshape(B, C, H//patch_size, patch_size, W//patch_size, patch_size)
+    x = x.permute(0, 2, 4, 1, 3, 5) # [B, H', W', C, p_H, p_W]
+    x = x.flatten(1,2)              # [B, H'*W', C, p_H, p_W]
+    if flatten_channels:
+        x = x.flatten(2,4)          # [B, H'*W', C*p_H*p_W]
+    # print(x.shape)
+    return x
