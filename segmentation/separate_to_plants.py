8c654ea5a56ab005e4fa1e0476e808c67efeb53f
jarsba <jarkko.lehtiranta@gmail.com>
2022-05-02 23:56:07 +0300

    Add working version from plant segmentation

--- segmentation/separate_to_plants.py
+++ segmentation/separate_to_plants.py
@@ -1,114 +1,10 @@
-# %%
-import os
-import numpy as np
-import cv2
-import matplotlib.pyplot as plt
-import glob
-import re
-from functools import cmp_to_key
-import pathlib
-from dotenv import load_dotenv
-import pandas as pd
-from typing import Tuple, List
-import logging
-from pathlib import Path
-# %%
 
-load_dotenv()
-
-logger = logging.getLogger(__name__)
-
-# %%
-
-DATA_FOLDER_PATH = os.getenv("DATA_FOLDER_PATH")
-
-# %%
-
-## Set the paths here before running
-input_path_of_tray_images = f'{DATA_FOLDER_PATH}/Data/Trial_01/Dataset_01/FishEyeMasked'
-output_path_for_separated_plants = f'{DATA_FOLDER_PATH}/Separated_plants/Trial_01/Dataset_01/Background_included'
-
-# %%
-# Remove little bits and pieces of other plants from this contour's bounding rectangle's corners
-def mask_plant_parts(img, return_only_mask=False):
-    contours = find_contours(img)
-    # longest contour usually corresponds to the whole leaf (not necessarily always)
-    i = np.argmax([len(c) for c in contours])
-    plant_contour = contours[i]
-    mask = np.zeros(img.shape[:2], np.uint8)
-    cv2.fillPoly(mask, pts=[plant_contour], color=(255, 255, 255))
-    if return_only_mask:
-        return mask
-    masked = cv2.bitwise_and(img, img, mask=mask)
-    return masked
-
-# %%
-def contour_sort(a, b):
-    br_a = cv2.boundingRect(a)
-    br_b = cv2.boundingRect(b)
-    a_x, a_y, a_w, a_h = br_a[0], br_a[1], br_a[2], br_a[3]
-    b_x, b_y, b_w, b_h = br_b[0], br_b[1], br_b[2], br_b[3]
-
-    # Check if a and be seem to be on the same "row" on the tray or not:
-    if((a_y - 100 <= b_y and a_y + a_h - 100 <= b_y) or (b_y - 100 <= a_y and b_y + b_h - 100 <= a_y)):
-        return a_y - b_y # Not on the same row
-    else:
-        return a_x - b_x # On the same row
-
-# %%
-def find_contours(img):
-    # Threshold input image using otsu thresholding as mask and refine with morphology
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU + cv2.THRESH_BINARY)[1]
-    # Use "close" morphological operation to close the gaps between contours
-    # Find contours in thresh_gray after closing the gaps
-    closed_gaps_thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (50,50)))
-    cnts = cv2.findContours(closed_gaps_thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
-
-    result = tuple(c for c in cnts if cv2.contourArea(c) > 500) # Filter contours smaller than 500 pixels out
-    return result
-
-
-# %%
-
-
-def segment_plant(masked_image_path: str, original_image_path: str, output_path: str) -> Tuple[List[str], List[str]]:
-    """Segment plant image to multiple segmented masked and segmented un-masked images"""
-    
-    masked_filename = Path(masked_image_path).stem
-    original_filename = Path(original_image_path).stem
-    
-    masked_filetype = os.path.splitext(masked_image_path)[1]
-    original_filetype = os.path.splitext(original_image_path)[1]
-    
-    pathname = os.path.join(output_path, masked_filename)
-
-    logger.info(f"Segmenting file {masked_filename}")
-    
-    masked_segmented_paths = []
-    original_segmented_paths = []
-    
-    if not os.path.exists(pathname):
-        os.makedirs(pathname)
-
-    masked_image = cv2.imread(masked_image_path)
-    original_image = cv2.imread(original_image_path)
-    
-    cnts = find_contours(masked_image)
-    sorted_cnts = sorted(cnts, key=cmp_to_key(contour_sort))
-    for contour_index, c in enumerate(sorted_cnts):
-
-        # Use the same mask for both masked and non-masked images
-
-        x, y, w, h = cv2.boundingRect(c)
-        ROI_masked = masked_image[y:y+h, x:x+w]
         ROI_original = original_image[y:y+h, x:x+w]
         plant_mask = mask_plant_parts(ROI_masked, return_only_mask=True)
         single_plant_masked = cv2.bitwise_and(ROI_masked, ROI_masked, mask=plant_mask).copy()
         single_plant_original = cv2.bitwise_and(ROI_original, ROI_original, mask=plant_mask).copy()
-        
-        # Write masked single plant 
+
+        # Write masked single plant
         masked_segmented_path = os.path.join(pathname, f"{masked_filename}_M{contour_index}{masked_filetype}")
         masked_segmented_paths.append(masked_segmented_path)
         cv2.imwrite(masked_segmented_path, single_plant_masked)
@@ -120,7 +16,7 @@ def segment_plant(masked_image_path: str, original_image_path: str, output_path:
 
 
     # for now, save the original image in the same location as the segments, just for easy checking that the segmentation has gone right
-    cv2.imwrite(os.path.join(pathname, f"{masked_filename}{masked_filetype}"), masked_image)        
+    cv2.imwrite(os.path.join(pathname, f"{masked_filename}{masked_filetype}"), masked_image)
     cv2.imwrite(os.path.join(pathname, f"{original_filename}{original_filetype}"), original_image)
 
     return masked_segmented_paths, original_segmented_paths
@@ -162,7 +58,7 @@ def test_segmentation():
 
             # Write the split plant in a file
             cv2.imwrite(f'{output_path_for_separated_plants}/{subfolder_name}/plant_index_{idx2+1}.png', result)
-        
+
     # %%
 
     gc_df = pd.read_csv(os.path.join(DATA_FOLDER_PATH, 'growth_chamber_plant_data.csv'))
